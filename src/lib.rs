6b7b5c2c2104a5f6bf8cb6f550486e8e021d9805
AregevDev <aregevdev@gmail.com>
2018-10-10 18:29:59 +0300

    Fixed duplicated module nesting

--- src/lib.rs
+++ src/lib.rs
@@ -3,45 +3,43 @@ mod os_name;
 mod arch;
 mod environment;
 
-pub mod desktop {
-    use os_name::ret_os_name;
-    use arch::ret_arch;
-    use environment::ret_environment;
+use os_name::ret_os_name;
+use arch::ret_arch;
+use environment::ret_environment;
 
-    // the main struct of the library, contains the OS information
-    #[derive(Debug)]
-    pub struct Desktop {
-        os_name: &'static str,
-        arch: &'static str,
-        environment: &'static str,
-    }
+// the main struct of the library, contains the OS information
+#[derive(Debug)]
+pub struct Desktop {
+    os_name: &'static str,
+    arch: &'static str,
+    environment: &'static str,
+}
 
-    impl Desktop {
-        // returns the target desktop information
-        pub fn get() -> Self {
-            Desktop {
-                os_name: ret_os_name(),
-                arch: ret_arch(),
-                environment: ret_environment(),
-            }
-        }
-
-        pub fn os_name(&self) -> &'static str {
-            self.os_name
-        }
-
-        pub fn arch(&self) -> &'static str {
-            self.arch
-        }
-
-        pub fn environment(&self) -> &'static str {
-            self.environment
+impl Desktop {
+    // returns the target desktop information
+    pub fn get() -> Self {
+        Desktop {
+            os_name: ret_os_name(),
+            arch: ret_arch(),
+            environment: ret_environment(),
         }
     }
 
-    #[test]
-    fn test_struct() {
-        let d = Desktop::get();
-        println!("{}", d.environment());
+    pub fn os_name(&self) -> &'static str {
+        self.os_name
     }
+
+    pub fn arch(&self) -> &'static str {
+        self.arch
+    }
+
+    pub fn environment(&self) -> &'static str {
+        self.environment
+    }
+}
+
+#[test]
+fn test_struct() {
+    let d = Desktop::get();
+    println!("{}", d.environment());
 }
