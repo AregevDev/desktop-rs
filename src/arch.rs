a7571567bb679b2ad1d89f70a0d6af35346df691
AregevDev <aregevdev@gmail.com>
2019-01-01 20:40:29 +0200

    Make the arch and the OS functions constant, more optimal

--- src/arch.rs
+++ src/arch.rs
@@ -1,23 +1,23 @@
 #[cfg(target_arch = "x86")]
-fn arch() -> &'static str {
+const fn arch() -> &'static str {
     "i686"
 }
 
 #[cfg(target_arch = "x86_64")]
-fn arch() -> &'static str {
+const fn arch() -> &'static str {
     "amd64"
 }
 
 #[cfg(target_arch = "arm")]
-fn arch() -> &'static str {
+const fn arch() -> &'static str {
     "arm"
 }
 
 #[cfg(target_arch = "mips")]
-fn arch() -> &'static str {
+const fn arch() -> &'static str {
     "mips"
 }
 
-pub fn ret_arch() -> &'static str {
+pub const fn ret_arch() -> &'static str {
     arch()
 }
--- src/os_name.rs
+++ src/os_name.rs
@@ -1,40 +1,40 @@
 #[cfg(target_os = "linux")]
-fn os_name() -> &'static str {
+const fn os_name() -> &'static str {
     "Linux"
 }
 
 #[cfg(target_os = "macos")]
-fn os_name() -> &'static str {
+const fn os_name() -> &'static str {
     "MacOS"
 }
 
 #[cfg(target_os = "windows")]
-fn os_name() ->  &'static str {
+const fn os_name() ->  &'static str {
     "Windows"
 }
 
 #[cfg(target_os = "ios")]
-fn os_name() -> &'static str {
+const fn os_name() -> &'static str {
     "IOS"
 }
 
 #[cfg(target_os = "android")]
-fn os_name() -> &'static str {
+const fn os_name() -> &'static str {
     "Android"
 }
 
 #[cfg(target_os = "freebsd")]
-fn os_name() ->  &'static str {
+const fn os_name() ->  &'static str {
     "FreeBSD"
 }
 
 #[cfg(target_os = "openbsd")]
-fn os_name() ->  &'static str {
+const fn os_name() ->  &'static str {
     "OpenBSD"
 }
 
 #[cfg(target_os = "netbsd")]
-fn os_name() ->  &'static str {
+const fn os_name() ->  &'static str {
     "NetBSD"
 }
 
